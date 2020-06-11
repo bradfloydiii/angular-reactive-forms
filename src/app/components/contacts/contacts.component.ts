0e0c00536723e589acbc46059417243a5ca0a2d6
bradfloydiii <bfloydiii@gmail.com>
2020-06-11 18:52:31 -0400

    wired backend

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from 'src/app/services/contact.service';
+import { IContact } from 'src/app/models/contact';
 
 @Component({
   selector: 'app-contacts',
@@ -13,9 +15,12 @@ export class ContactsComponent implements OnInit {
   phone = '+1 (906) 585-2525';
   address = '920 Hastings Street, Roosevelt, Puerto Rico, 5573';
 
-  constructor() { }
+  contacts: IContact[] = [];
+
+  constructor(private contactService: ContactService) {}
 
   ngOnInit(): void {
+    this.contactService.getContacts().subscribe(contacts => this.contacts = contacts);
   }
 
 }
