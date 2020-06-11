3c69bbcaf2480c30f43f2af5e777f8eaff69b42b
bradfloydiii <bfloydiii@gmail.com>
2020-06-10 22:11:19 -0400

    adding in angular material

--- src/app/components/add-contacts/add-contacts.component.ts
+++ src/app/components/add-contacts/add-contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contacts',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddContactsComponent implements OnInit {
 
+  formGroup: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  initForm() {
+    this.formGroup = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      company: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      phone: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      zipCode: new FormControl('', [Validators.required]),
+    });
   }
 
 }
